6e7a3e67575d3c96b3cce46774af65acd8af1c1d
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-06 20:52:39 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
+
+export const App = () => {
+  console.log("App 랜더링");
+
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum((prev) => prev + 1);
+  };
+
+  return (
+    <>
+      <button onClick={onClickButton}>버튼</button>
+      <p>{num}</p>
+      <Child1 />
+      <Child4 />
+    </>
+  );
+};
